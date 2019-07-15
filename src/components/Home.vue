1b453285ec4d640d4a84f6e8189a0e48aeba757f
Lahiru Chathuranga <lahiru.b@creativesoftware.com>
2019-07-15 16:47:24 +0530

    save

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -18,8 +18,8 @@
                     id
                     class="text-search"
                     placeholder="job title, keywords, or company"
-                  >
-                  <input type="button" value="Search" class="btn-search">
+                  />
+                  <input type="button" value="Search" class="btn-search" />
                 </div>
                 <div class="post-job">
                   <a href="#">
@@ -51,7 +51,7 @@
       <div class="row clearfix">
         <div class="col-md-12">
           <div class="category-section">
-            <div class="section-title">Recommended jobs</div>
+            <div class="section-title">Trending Searches</div>
             <div class="section-content">
               <div class="category-set">
                 <div class="category-item">
@@ -140,22 +140,42 @@
       <div class="row">
         <div class="col-md-12">
           <div class="recommended">
-            <div class="section-title">Recommended jobs</div>
+            <div class="section-title">
+              Recommended jobs
+              <div class="list-type">
+                <div class="btn-type-tile" v-bind:class="{'active':this.gridTypeList==false}" @click="setTileType()"></div>
+                <div class="btn-type-list" v-bind:class="{'active':this.gridTypeList==true}" @click="setListType()"></div>
+              </div>
+            </div>
             <div class="section-content">
-              <div class="rec-set">
+              <div class="rec-set" v-bind:class="{'list':this.gridTypeList==true}">
                 <div class="rec-item" v-for="index in 60" v-bind:key="index">
                   <div class="rec-box">
                     <a href="#">
                       <div class="comp-logo">
                         <div class="image">
-                          <img src="../assets/company-logo.png" alt>
+                          <img src="../assets/company-logo.png" alt />
                         </div>
                       </div>
-                      <div class="comp-details">
+                      <div class="comp-details clearfix">                          
+                        <div class="list-col-left">
+                            <div class="company-details">
                         <div class="name">Interaction Designer</div>
                         <div class="company">Salesforce</div>
+                        
+                        </div>
+                        
                         <div class="location">Colombo, Sri Lanka</div>
-                        <div class="ago">1 week ago</div>
+                        </div>
+                        <div class="list-col-right">
+                        <div class="ago">
+                            <div v-if="gridTypeList==false">1 week ago</div>
+                            <div v-if="gridTypeList==true">
+                            <div class="posted-date"><span class="ago-label">Posted on</span>21-03-2019</div>
+                            <div class="expired-date"><span class="ago-label">Expiry Date</span>21-03-2019</div>
+                            </div>
+                        </div>
+                        </div>
                       </div>
                     </a>
                   </div>
@@ -191,69 +211,78 @@
       </div>
     </div>
     <div class="footer-container">
-<div class="container">
-  <div class="row">
-    <div class="col-md-12">
-      <div class="logo-row">
-        <img src="../assets/logo.svg" alt="">
-      </div>
-      <div class="col-row">
-      <div class="f-col-5">
-        <div class="f-col">
-          <div class="col-title">jobbly.lk</div>
-          <div class="nav-list">
-            <a href="">About us</a>
-            <a href="">Blog</a>
-            <a href="">Sitemap</a>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="logo-row">
+              <img src="../assets/logo.svg" alt />
+            </div>
+            <div class="col-row clearfix">
+              <div class="f-col-5">
+                <div class="f-col">
+                  <div class="col-title">jobbly.lk</div>
+                  <div class="nav-list">
+                    <a href>About us</a>
+                    <a href>Blog</a>
+                    <a href>Sitemap</a>
+                  </div>
+                </div>
+              </div>
+              <div class="f-col-5">
+                <div class="f-col">
+                  <div class="col-title">Job Seekers</div>
+                  <div class="nav-list">
+                    <a href>Browse jobs</a>
+                    <a href>Upload resume</a>
+                    <a href>Create an account</a>
+                  </div>
+                </div>
+              </div>
+              <div class="f-col-5">
+                <div class="f-col">
+                  <div class="col-title">Employers</div>
+                  <div class="nav-list">
+                    <a href>Post a job</a>
+                    <a href>Search resume</a>
+                    <a href>My dashboard</a>
+                  </div>
+                </div>
+              </div>
+              <div class="f-col-5">
+                <div class="f-col">
+                  <div class="col-title">Community</div>
+                  <div class="nav-list">
+                    <a href>Help/Contact us</a>
+                    <a href>Terms of use</a>
+                    <a href>Privacy policy</a>
+                    <a href>Guidelines</a>
+                    <a href>FAQs</a>
+                    <a href>Cookie Notice</a>
+                  </div>
+                </div>
+              </div>
+              <div class="f-col-5">
+                <div class="f-col">
+                  <div class="col-title">Work with us</div>
+                  <div class="nav-list">
+                    <a href>Careers</a>
+                    <a href>Advertise</a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-        <div class="f-col-5">
-        <div class="f-col">
-           <div class="col-title">Job Seekers</div>
-          <div class="nav-list">
-            <a href="">Browse jobs</a>
-            <a href="">Upload resume</a>
-            <a href="">Create an account</a>
+      <div class="rights-row">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="right">© 2019 Jobbly. All rights reserved.</div>
+            </div>
           </div>
         </div>
       </div>
-        <div class="f-col-5">
-        <div class="f-col">
-           <div class="col-title">Employers</div>
-          <div class="nav-list">
-            <a href="">Post a job</a>
-            <a href="">Search resume</a>
-            <a href="">My dashboard</a>
-          </div>
-        </div>
-      </div>
-        <div class="f-col-5">
-        <div class="f-col">
-           <div class="col-title">Community</div>
-          <div class="nav-list">
-            <a href="">Help/Contact us</a>
-            <a href="">Terms of use</a>
-            <a href="">Privacy policy</a>
-            <a href="">Guidelines</a>
-            <a href="">FAQs</a>
-            <a href="">Cookie Notice</a>
-          </div>
-        </div>
-      </div>
-        <div class="f-col-5">
-        <div class="f-col">
-           <div class="col-title">Work with us</div>
-          <div class="nav-list">
-            <a href="">Careers</a>
-            <a href="">Advertise</a>
-          </div>
-        </div>
-      </div>
-      </div>
-    </div>
-  </div>
-</div>
     </div>
   </div>
 </template>
@@ -261,8 +290,21 @@
 <script>
 import Menu from "./Menu.vue";
 export default {
+    data(){
+        return{
+            gridTypeList:false
+        }
+    },
   components: {
     Menu: Menu
+  },
+  methods:{
+      setListType:function(){
+          this.gridTypeList=true;
+      },
+      setTileType:function(){
+          this.gridTypeList=false;
+      }
   }
 };
 </script>
@@ -274,10 +316,10 @@ export default {
 }
 
 .container.section {
-  margin: 24px auto;
+  margin: 40px auto;
   .section-title {
     font-size: 16px;
-    margin-bottom: 14px;
+    margin-bottom: 20px;
     color: $font-color;
     font-weight: 600;
     text-align: left;
@@ -289,7 +331,7 @@ export default {
   }
 }
 .container.search {
-  margin-top: 24px;
+  margin-top: 40px;
   .search-box-container {
     width: 100%;
     // height: 280px;
@@ -384,22 +426,30 @@ export default {
             .icon-arrow {
               display: block;
               position: absolute;
-              top: 7px;
-              right: -15px;
+              top: 6px;
+              right: -16px;
               width: 12px;
               height: 12px;
               background-image: url("../assets/icon-arrow-right.svg");
               background-repeat: no-repeat;
-              background-size: contain;
+              //   background-size: contain;
+              @include transition(right 0.1s ease-in-out);
             }
 
             &:hover {
+              color: $secondary-theme-color;
               .icon-arrow {
-                right: -17px;
+                background-position-y: -12px;
+                right: -18px;
               }
             }
           }
         }
+      }
+
+      @media screen and (max-width: 768px) {
+        width: 100%;
+        height: auto;
       }
     }
     .col-details {
@@ -411,6 +461,10 @@ export default {
       color: $secondary-font-color;
       .details-section {
         padding: 40px;
+        @media only screen and (max-width: 991px) and (min-width: 768px) {
+             padding:20px !important;
+         }
+        
 
         .resume-description {
           margin: 10px 0 20px 0;
@@ -460,6 +514,14 @@ export default {
             }
           }
         }
+        
+      }
+       @media screen and (max-width: 768px) {
+        width: 100%;
+        height: auto;
+
+        
+      border-radius: 0 0 6px 6px;
       }
     }
   }
@@ -473,6 +535,19 @@ export default {
     display: block;
     float: left;
     width: 20%;
+      @media only screen and (max-width: 1200px) and (min-width: 991px) {
+           
+    width: 25%;
+         }
+    @media only screen and (max-width: 990px) and (min-width: 768px) {
+           
+    width: 33.33%;
+         }
+           @media only screen and (max-width: 767px) and (min-width: 342px) {
+           
+    width: 50%;
+         }
+
     a {
       display: block;
       color: #365669;
@@ -487,10 +562,12 @@ export default {
       text-overflow: ellipsis;
       overflow: hidden;
       white-space: nowrap;
+    border: 1px solid #e4ecfb;
       &:hover {
         text-decoration: none;
         color: $secondary-theme-color;
         background: #ffe5ec;
+        border-color: #ffcfdb;
       }
     }
     &.all-categories {
@@ -504,6 +581,40 @@ export default {
 }
 
 .recommended {
+  .section-title {
+    position: relative;
+    .list-type {
+      position: absolute;
+      top: 0;
+      right: 6px;
+
+      .btn-type-tile {
+        width: 20px;
+        height: 20px;
+        background-image: url("../assets/icon-grid-type.png");
+        background-position: 0px 0px;
+        float: left;
+        margin-right: 8px;
+        cursor: pointer;
+        &:hover,
+        &.active {
+          background-position: 0px -20px;
+        }
+      }
+      .btn-type-list {
+        width: 20px;
+        height: 20px;
+        background-image: url("../assets/icon-grid-type.svg");
+        background-position: 20px 0px;
+        float: left;
+        cursor: pointer;
+        &:hover,
+        &.active {
+          background-position: -20px -20px;
+        }
+      }
+    }
+  }
   .rec-set {
     .rec-item {
       width: 25%;
@@ -565,55 +676,141 @@ export default {
         }
       }
     }
+
+    &.list{
+        .rec-item{
+      width: 100%;
+
+      .rec-box {
+        padding: 3px;
+        a {
+          display: block;
+          width: 100%;
+          position: relative;
+          padding-left: 60px;
+          text-decoration: none;
+          background-color: #ffffff;
+          border: 1px solid #e4ecfb;
+          border-radius: 4px;
+          color: inherit;
+          text-align: left;
+        }
+        .comp-logo {
+          width: 60px;
+          height: 100%;
+          position: absolute;
+          left: 0;
+          .image {
+            width: 38px;
+            height: 38px;
+            background-color: #d6dbe4;
+            border-radius: 100px;
+            margin: 5px auto;
+            img {
+              width: 100%;
+            }
+          }
+        }
+        .comp-details {
+          padding-left: 6px;
+          padding-top: 10px;
+
+          .list-col-left{
+              width: 50%;
+              float: left;
+              margin-top: -6px;
+
+              .company-details{
+                  width: 70%;
+    float: left;
+          .name {
+            font-size: 12px;
+            font-weight: 600;
+            color: $font-color;
+          }
+          .company {
+            font-size: 12px;
+            font-weight: 600;
+            color: #ff194e;
+          }}
+           .location {
+            font-size: 12px;
+            color: $font-color;
+            float: left;
+    margin-top: 10px;
+          }
+          }
+          .list-col-right{
+              width: 50%;
+              float: left;
+         
+          .ago {
+            font-size: 10px;
+            text-align: right;
+            padding: 0 10px 10px 0;
+            padding-bottom: 10px;
+            color: $font-color;
+
+            .ago-label{
+                font-weight: 600;
+                margin-right: 4px;
+            }
+          }
+          }
+        }
+      }
+      }
+    }
   }
 }
 
 .post-jobs-container {
   .post-jobs {
     background-color: $font-color;
-  border-radius: 6px;
-  color: $secondary-font-color;
-  
+    border-radius: 6px;
+    color: $secondary-font-color;
+
     height: 100%;
     .col-details {
       width: 70%;
       float: left;
       .details-section {
-        
-    // padding: 40px;
-    padding: 60px 40px;
+        // padding: 40px;
+        padding: 60px 40px;
+
 
         .title {
           font-size: 16px;
           font-weight: 600;
         }
         .sub-title {
-          font-size: 30px;
+          font-size: 26px;
           font-weight: 600;
+          color: #00fdff;
         }
         .desc {
           font-size: 14px;
         }
         .submit-job {
-          margin-top:20px;
+          margin-top: 20px;
           .btn-post-job {
-              background-color: transparent;
-    border: 1px solid #fff;
-    color: #fff;
-    padding: 10px 40px;
-    border-radius: 4px;
-    font-size: 14px;
-    outline: none;
-        }
+            background-color: transparent;
+            border: 1px solid #00fdff;
+            color: #00fdff;
+            padding: 10px 40px;
+            border-radius: 4px;
+            font-size: 14px;
+            outline: none;
+          }
         }
       }
     }
 
-    .col-image{
+    .col-image {
       width: 30%;
-    height: 100%;
+      height: 100%;
       float: left;
-      background-image: url('../assets/professional2.jpg');
+      background-image: url("../assets/professional2.jpg");
       background-size: cover;
       background-repeat: no-repeat;
       border-radius: 0 6px 6px 0;
@@ -621,47 +818,60 @@ export default {
   }
 }
 
-.footer-container{
+.footer-container {
   background-color: #fff;
   min-height: 300px;
 
-  .logo-row{    margin: 40px 0 20px 0;
-    img{
-      width:140px;
+  .logo-row {
+    margin: 40px 0 20px 0;
+    img {
+      width: 140px;
     }
   }
 
-  .col-row{
-    .f-col-5{
+  .col-row {
+    .f-col-5 {
       width: 20%;
       float: left;
 
-      .f-col{
+      .f-col {
         padding: 10px;
 
-        .col-title{
+        .col-title {
           font-size: 16px;
           font-weight: 600;
           margin-bottom: 6px;
+          color: $font-color;
         }
-        .nav-list{
-
-          a{
-            display:block;
-            color:$font-color;
+        .nav-list {
+          a {
+            display: block;
+            color: $font-color;
             font-size: 14px;
-    line-height: 26px;
+            line-height: 26px;
             text-decoration: none;
+            @include transition(all 0.1s ease-in-out);
+
+            &:hover {
+              color: $secondary-theme-color;
+            }
           }
         }
       }
 
-      &:first-child{
-         .f-col{
-        padding-left: 0px;
-      }
+      &:first-child {
+        .f-col {
+          padding-left: 0px;
+        }
       }
     }
   }
+}
+.rights-row {
+  font-size: 12px;
+  padding: 20px 0;
+  border-top: 1px solid #eceeef;
+  color: $font-color;
+  margin-top: 40px;
 }
 </style>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -9,13 +9,13 @@
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav class="center">
+      <b-navbar-nav class="menu-items">
         <b-nav-item href="#"><span class="icon briefcase"></span><span class="nav-label">Find a job</span></b-nav-item>
         <b-nav-item href="#"><span class="icon manager"></span><span class="nav-label">For employers</span></b-nav-item>
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
+      <b-navbar-nav class="ml-auto right-menu">
 
 
       <b-nav-item href="#" class="highlight">Post job free</b-nav-item>
@@ -35,10 +35,11 @@ export default {
 </script>
 
 <style lang="scss" >
+@import "../scss/custom.scss";
 .main-menu{
   
-    // font-weight: 600;
     font-size: 14px;
+    font-weight: 600;
 
 .navbar-nav{
   .nav-item{
@@ -57,19 +58,20 @@ export default {
       .nav-label{
         display: block;
         float: left;
-        line-height: 40px;}
+        line-height: 40px;
+    color:$font-color;}
     }
 
     &.highlight{
-          background-color: #00639f;
-    border-radius: 22px;
-    padding: 0 10px;
-    padding: 4px 4px 5px 4px;
-    color: #ffffff;
-    margin-right: 6px;
 
     a{
-      
+          color: #ffffff;
+    background-color: #00639f;
+    border-radius: 22px;
+    padding: 0 10px;
+    padding: 4px 14px 5px 14px;
+    color: #ffffff;
+    margin-right: 6px;
     color: #ffffff;
     }
     }
@@ -78,10 +80,81 @@ export default {
       padding: 4px 0 5px 0;
     }
   }
-  &.center{
-    margin:0 auto;
+  &.menu-items{
+    margin-left: 20px;
   }
+&.right-menu{
+  margin-right: 16px;
 }
+  
+}
+.navbar-toggler{
+    border:none;
+    outline: none;
+    padding: 5px 8px;
+    border: 1px solid #ffaabe;
+    margin-right: 16px;
+    
+      border-color: #ffaabe !important;
+    .navbar-toggler-icon{
+      background-image: url('../assets/icon-navbar-toggle.svg');
+      background-repeat: no-repeat;
+    background-size: initial;
+    }
 
+    &:hover{
+    background-color: #fff2f5;
+      border-color: #ffaabe;;
+    }
+  }
+@media screen and (max-width: 991px) {
+
+  .navbar-collapse{
+        border-top: 1px solid #d5ddec;
+    margin-top: 10px;
+    
+    padding: 10px;
+
+    .navbar-nav.menu-items{
+      margin-left: 0;
+
+      .nav-item{
+        
+        padding: 5px 0;
+        a{    display: block;
+    clear: both;
+    content: ""; overflow: auto;
+    padding: 0;
+        }
+
+        
+      }
+    }
+
+    .navbar-nav.right-menu{
+      
+      .nav-item{
+        
+    padding: 16px 0;
+
+
+a{
+  
+        color: $font-color;
+}
+    &.highlight{
+
+    a{
+        background-color: transparent;
+        color: $font-color;
+        padding:0px 10px;;
+    }
+    }
+
+      }
+
+    }
+  } 
+}
 }
 </style>
